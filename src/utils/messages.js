aa0778a8695fbc37ca5a273423e5b2790f0dd5bc
Luiz Fellype <luiz.cassago@xdevel.com.br>
2019-03-20 22:16:07 -0300

    feat: add messages and field validations

---
+++ src/utils/messages.js
@@ -0,0 +1,27 @@
+export const MESSAGES = {
+  ERRORS: {
+    GENERIC: {
+      TITLE: 'Ocorreu um erro!'
+    },
+    AUTH: {
+      LOGIN: {
+        FAIL_TITLE: 'Falha no login',
+        FAIL: 'Usuário ou senha incorreto(s)',
+        INVALID_EMAIL: 'Email inválido',
+        EMPTY_PASSWORD: 'Por favor insira a senha'
+      }
+    }
+  },
+  SUCCESS: {
+    GENERIC: {
+      TITLE: 'Operação realizada com sucesso!'
+    },
+    AUTH: {
+      LOGIN: 'Login efetuado. Redirecionando...'
+    },
+    SERVICE: {
+      RULEDELETED: 'Agenda deletada!',
+      RULECREATED: 'Agenda criada!'
+    }
+  }
+}
---
+++ src/utils/validations.js
@@ -0,0 +1,4 @@
+export const validations = {
+  isEmail: str =>
+    /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(str)
+}
