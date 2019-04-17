376d088474616f9fcc78788f9a4908313ee49d25
Luiz Fellype <luiz.cassago@xdevel.com.br>
2019-04-17 20:37:58 -0300

    feat: init home page with the form

---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ProductForm } from '../../containers'
+import './Home.css'
+
+// eslint-disable-next-line react/display-name
+export const Home = React.memo(() => {
+  return (
+    <>
+      <ProductForm />
+    </>
+  )
+})
