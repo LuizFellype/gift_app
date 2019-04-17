34617075cc20d6713ea821532144a79dbaa9bf53
Luiz Fellype <luiz.cassago@xdevel.com.br>
2019-04-17 20:35:02 -0300

    feat: add input component

---
+++ src/components/input/Input.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { InputText } from 'primereact/inputtext'
+
+export const useInput = (inputProps, initialValue = '') => {
+  const [value, setvalue] = useState(initialValue)
+
+  return {
+    value,
+    onChange: e => setvalue(e.currentTarget.value),
+    ...inputProps
+  }
+}
+
+export const Input = props => {
+  return (
+    <div className='input-block'>
+      <label htmlFor={props.id}>{props.label}</label>
+      <InputText {...props} />
+    </div>
+  )
+}
